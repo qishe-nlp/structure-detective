1b737926b5e2f85264b7bc81a8fe158efa8dfb2e
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2022-01-09 11:38:11 +0800

    update version

--- structure_detective/__init__.py
+++ structure_detective/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .constants import PKG_INDICES
 from .structure import Structure, trs
--- tests/test_structure_detective.py
+++ tests/test_structure_detective.py
@@ -2,4 +2,4 @@ from structure_detective import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.1'
